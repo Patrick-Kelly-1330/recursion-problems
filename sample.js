e2fb1ae0766b87a6dc16c49772db9d4c220a68f0
Patrick Kelly <patrick.will.kelly@gmail.com>
2022-10-29 15:57:12 -0700

    object update

--- sample.js
+++ sample.js
@@ -1,1 +1,18 @@
+var sampleNestedObject = {
+  firstName: 'John',
+  lastName: 'Williams',
+  age: 45,
+  children: [{
+    firstName: 'Sarah',
+    age: 12,
+    }, {
+    firstName: 'Mike',
+    age: 15,
+  }],
+  firstName: 'Jach',
+  lastName: 'Samson',
+  age: 42,
+  children: [];
+};
+
 
